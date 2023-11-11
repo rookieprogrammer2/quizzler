4daf1ca29df972752738a38f32a652faf2f38120
LENOVO <ABurger2021@Oultook.com>
2023-11-11 09:03:01 +0200

    Linked the Firebase project with this Flutter project.

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:quizzler/ui/screens/login_sc.dart';
 import 'package:quizzler/ui/screens/pass_reset_sc.dart';
 import 'package:quizzler/ui/screens/register_sc.dart';
 import 'package:quizzler/ui/screens/splash_sc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   return runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
