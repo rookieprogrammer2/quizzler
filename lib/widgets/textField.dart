fcafd795c162c3364bf89ab9d23f37c84168eddb
LENOVO <ABurger2021@Oultook.com>
2023-12-30 20:47:38 +0200

    Modified the design for the login and register screens for the final time -hopefully. Added hide/show password functionality.

--- lib/widgets/textField.dart
+++ lib/widgets/textField.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 typedef Validator = String? Function(String?);
 
@@ -10,6 +9,10 @@ class MyTextFormField extends StatelessWidget {
   final IconButton? suffixIcon;
   final TextInputType keyboardType;
   final TextEditingController? textEditingController;
+  final void Function(String)? onChanged;
+  final Icon textFieldIcon;
+  final BuildContext? context;
+
   const MyTextFormField(
       {super.key,
       this.textEditingController,
@@ -17,7 +20,10 @@ class MyTextFormField extends StatelessWidget {
       this.isObscure = false,
       this.suffixIcon,
       required this.keyboardType,
-      required this.validator});
+      required this.validator,
+      this.onChanged,
+      required this.textFieldIcon,
+      this.context});
 
   @override
   Widget build(BuildContext context) {
@@ -28,39 +34,49 @@ class MyTextFormField extends StatelessWidget {
         horizontal: width * 0.001,
         vertical: height * 0.01,
       ),
-      child: TextFormField(
-        style: const TextStyle(color: Colors.white),
-        validator: validator,
-        controller: textEditingController,
-        keyboardType: keyboardType,
-        obscureText: isObscure,
-        decoration: InputDecoration(
-          enabledBorder: const UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.white, width: 0.9),
+      child: Row(
+        children: [
+          textFieldIcon,
+          SizedBox(width: width * 0.02),
+          Expanded(
+            child: TextFormField(
+              style: const TextStyle(
+                  color: Colors.white,
+              ),
+              validator: validator,
+              controller: textEditingController,
+              keyboardType: keyboardType,
+              obscureText: isObscure,
+              onChanged: onChanged,
+
+              decoration: InputDecoration(
+                border: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 1)),
+                enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white, width: 1),
+                ),
+                focusedBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white, width: 1),
+                ),
+                errorBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red.shade600, width: 1),
+                ),
+                focusedErrorBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.yellow.shade300, width: 1),
+                ),
+
+                suffixIcon: suffixIcon,
+                hintText: hintText,
+                hintStyle: TextStyle(
+                  color: Colors.white.withOpacity(0.7),
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14,
+                ),
+              ),
+            ),
           ),
-          border: const UnderlineInputBorder(),
-          suffixIcon: isObscure
-              ? IconButton(
-                  onPressed: null,
-                  icon: Icon(
-                    Icons.remove_red_eye_outlined,
-                    color: Colors.white,
-                  ),
-                )
-              : IconButton(
-                  onPressed: null,
-                  icon: SvgPicture.asset(
-                    "assets/icons/visibility_off.svg",
-                    color: Colors.white,
-                    width: width * 0.06,
-                  )),
-          hintText: hintText,
-          hintStyle: const TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w400,
-            fontSize: 15,
-          ),
-        ),
+        ],
       ),
     );
   }
